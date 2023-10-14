dbdd17d672152f4ade57c0052bb9fbf744269e4b
Anderson Souza <andersonsouza007@live.com>
2023-10-14 16:29:52 -0300

    Formulário de cadastro de usuário

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -9,6 +9,7 @@ import { useState } from "react";
 import Loader from "../Components/UI/Loader"
 
 const Login = ()=>{
+    const [form, setForm] = useState(0);
     const {setUserLogged} = useLogged();
     const [loading, setLoading] = useState(false);
     const {handleSetAlert} = useAlert();
@@ -34,17 +35,49 @@ const Login = ()=>{
     return(
         <>
         {loading && <Loader />}
+
         <div className="container-fluid p-2 flex h-screen items-center justify-center bg-danger">
+        <div>
+
+        {form == 0 &&
             <form className="" onSubmit={handleSubmit(authenticate)}>
                 <div className="justify-center items-start bg-purple-950 text-white rounded-lg px-8 py-8 flex flex-col gap-3 form-group">
                     <h1 className="text-3xl text-center">ToDo</h1>
-                    <Input defaultValue="" autofocus="autofocus" placeholder="Digite seu usuário" rules={{required:"Este campo é obrigatório."}} type="text" label="Usuário" name="user" control={control} />
-                    <Input defaultValue="" placeholder="Digite sua senha"  rules={{required:"Este campo é obrigatório."}} type="password" label="Senha" name="password" control={control} />
+                    <Input defaultValue="" autofocus="autofocus" placeholder="Digite seu usuário" rules={{required:"Este campo é obrigatório.", maxLength:{value:"255", message:"Este campo só suporta 255 caracteres"}}} type="text" label="Usuário" name="user" control={control} />
+                    <Input defaultValue="" placeholder="Digite sua senha"  rules={{required:"Este campo é obrigatório.", maxLength:{value:"255", message:"Este campo só suporta 255 caracteres"}}} type="password" label="Senha" name="password" control={control} />
                     <div>
                         <button className="btn btn-primary">Login</button>
                     </div>
                 </div>
             </form>
+        }
+        {form == 1 &&
+            <form className="" onSubmit={handleSubmit(authenticate)}>
+            <div className="justify-center items-start bg-purple-950 text-white rounded-lg px-8 py-8 flex flex-col gap-3 form-group">
+                <h1 className="text-3xl text-center">ToDo</h1>
+                <div className="form-group w-100">
+                    <Input  defaultValue="" placeholder="Digite seu nome"  rules={{required:"Este campo é obrigatório.", maxLength:{value:"255", message:"Este campo só suporta 255 caracteres"}}} type="password" label="Nome" name="name" control={control} />
+                </div>
+                <div className="form-group flex gap-2">
+                    <Input defaultValue="" autofocus="autofocus" placeholder="Digite seu usuário" rules={{required:"Este campo é obrigatório.", maxLength:{value:"255", message:"Este campo só suporta 255 caracteres"}}} type="text" label="Usuário" name="user" control={control} />
+                    <Input defaultValue="" placeholder="Digite sua senha"  rules={{required:"Este campo é obrigatório.", maxLength:{value:"255", message:"Este campo só suporta 255 caracteres"}}} type="password" label="Senha" name="password" control={control} />
+                </div>
+                <div>
+                    <button className="btn btn-primary">Cadastrar</button>
+                </div>
+            </div>
+        </form>
+        }
+        <div>
+            {form == 0 &&
+                <p className="btn btn-sm btn-light mt-2 cursor-pointer" onClick={()=> setForm(form + 1)} >Cadastre-se</p>
+            }
+            {form == 1 &&
+                <p className="btn btn-sm btn-light mt-2 cursor-pointer" onClick={()=> setForm(form - 1)} >Já tenho cadastro</p>
+            }
+        </div>
+        </div>
+        
         </div>
         </>
     )
