5444372637a48844ab0267be49dc88af6ab3dd7f
Anderson Souza <andersonsouza007@live.com>
2023-10-21 16:28:33 -0300

    dnd

--- src/Components/UI/Task/index.jsx
+++ src/Components/UI/Task/index.jsx
@@ -5,8 +5,7 @@ import hookRemoveTask from "../../../hooks/hookRemoveTask";
 
 import ConfirmationScreen from "../ConfirmationScreen";
 
-export default function Task({task,onDragEnter,onDragOver, onDragEnd, onDragStart, detailsShow, className, onClick,done}){
-
+export default function Task({task,onDrag,onDropTask, detailsShow, className, onClick,done}){
     const priorities = ['Baixa', 'Média', 'Alta'];
     const themeCtx = useTheme();
     const {removeTask} = hookRemoveTask();
@@ -27,12 +26,7 @@ export default function Task({task,onDragEnter,onDragOver, onDragEnd, onDragStar
             <ConfirmationScreen showConfirmationScreen={showConfirmationScreen}  setShowConfirmationScreen={handleDeleteRegister} id={idTaskToDelete} onClick={removeTask} />        
         }
         
-        <div  draggable key={task.id} 
-        onDragStart={onDragStart}  
-        onDragOver={onDragOver} 
-        onDragEnd={onDragEnd} 
-        onDragEnter={onDragEnter} 
-        className={`flex cursor-move flex-wrap gap-2 rounded-md  flex-row border p-2 mt-4 ${className}`}>
+        <div draggable key={task.id} onDrop={onDropTask} onDrag={onDrag} className={`flex cursor-move flex-wrap gap-2 rounded-md  flex-row border p-2 mt-4 ${className}`}>
             <h6 className={`${task.priority == 0 ? `${themeCtx.theme == 'dark' ? 'bg-blue-700':'bg-blue-700 text-white'}`:task.priority == 1 ? `${themeCtx.theme == 'dark' ? 'bg-yellow-700 text-white':'bg-yellow-700 text-white'}`:themeCtx.theme == 'dark' ? 'bg-red-600':'bg-red-600 text-white'} p-1 rounded max-h-7`}>Prioridade: {priorities[task.priority]}</h6>
             <div onClick={() => onClick(task.id)} className="flex w-100 flex-col">
                 <h5 className="fw-bold text-sm">{task.title}</h5>
--- src/views/app/pages/ByPriority.jsx
+++ src/views/app/pages/ByPriority.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
+import HTTP from "../../../api/http";
 import Task from "../../../Components/UI/Task";
+import { useAlert } from "../../../Contexts/AlertContext";
 import { useTheme } from "../../../Contexts/ContextsLoaders/useTheme";
 import { useTasks } from "../../../Contexts/TasksContext"
 import hookDoneTask from "../../../hooks/hookDoneTask";
@@ -8,23 +10,27 @@ import hookShowDetails from "../../../hooks/hookShowDetails";
 
 export default function ByPriority(){
     const {done} = hookDoneTask();
+    const {handleSetAlert} =  useAlert();
+
+    const [taskDragged, setTaskDragged] = useState('');
+    const {setTask} = useTasks();
     const [showOnly, setShowOnly] = useState([0,1,2]);
     const themeCtx = useTheme();
     const [showInfoTask, setShowInfoTask] = useState([0,1,2]);
     const {tasks} = useTasks();
     const {detailsShow, showDetails} = hookShowDetails();
     const baixa = tasks.filter((task)=>{
-        if(task.priority == 0){
+        if(task.priority == 0 && task.done == 0){
             return task;
         }
     })
     const media = tasks.filter((task)=>{
-        if(task.priority == 1){
+        if(task.priority == 1 && task.done == 0){
             return task;
         }
     })
     const alta = tasks.filter((task)=>{
-        if(task.priority == 2){
+        if(task.priority == 2 && task.done == 0){
             return task;
         }
     })
@@ -42,7 +48,26 @@ export default function ByPriority(){
             setShowOnly([id])
         }
     }
-
+    const onDrag = (e, task)=>{
+        e.preventDefault();
+        setTaskDragged({dragged: task})
+    }
+    const onDrop = async (e, priority)=>{
+        const http = new HTTP('/admin/tasks/changepriority/'+taskDragged.dragged+'/'+priority)
+        const changePriority = await http.http();
+        if(changePriority.error == false){
+            handleSetAlert({type:"success", message:changePriority.message})
+            setTask(true)
+        }else{
+            handleSetAlert({type:"danger", message:changePriority.message})
+        }
+    }
+    const onDragOver = (e)=>{
+        e.preventDefault();
+    }
+    const onDropTask = (e, prev)=>{
+        
+    }
     return(
         <div style={{maxHeight:"100vh", height:"calc(100vh - 60px)", overflowY:"auto", overflowX:"hidden"}} className="">
             <div className="flex justify-center mb-4 gap-2">
@@ -55,33 +80,33 @@ export default function ByPriority(){
             
             <div style={{overflow:"hidden"}} className="row gap-2 justify-center">
                 {showOnly.includes(0) &&
-                <div className="col-lg-3 bg-blue-700 rounded-lg p-4 col-sm-6">
+                <div onDrop={(e)=> onDrop(e, 0)} onDragOver={(e) => onDragOver(e) } className="col-lg-3 bg-blue-700 rounded-lg p-4 col-sm-6">
                     <div onClick={()=> openInformation(0)} className={`${themeCtx.theme == 'dark' ? "bg-blue-700":"bg-blue-700 text-white"} p-2 cursor-pointer rounded text-center fw-bold`}>Baixa</div>
                     {showInfoTask.includes(0) &&
                         baixa.map((task)=>{
                             return (
-                                <Task  key={task.id + task.title} done={done} onClick={showDetails} className={`${themeCtx.theme == 'dark' ? 'dark':'bg-light'} `} detailsShow={detailsShow}  task={task} />
+                                <Task onDragOver={(e)=> onDragOver(e)}  onDrag={(e)=> onDrag(e, task.id)} key={task.id + task.title} done={done} onClick={showDetails} className={`${themeCtx.theme == 'dark' ? 'dark':'bg-light'} `} detailsShow={detailsShow}  task={task} />
                                 )
                             })
                         }
                 </div>
                 }
                 {showOnly.includes(1) &&
-                <div className="col-lg-3 overflow-y-auto  bg-yellow-700 p-4 col-sm-6 rounded-lg">
+                <div onDrop={(e)=> onDrop(e, 1)} onDragOver={(e) => onDragOver(e) } className="col-lg-3 overflow-y-auto  bg-yellow-700 p-4 col-sm-6 rounded-lg">
                     <div onClick={()=> openInformation(1)} className={`${themeCtx.theme == 'dark' ? "bg-yellow-700 text-white":"bg-yellow-700 text-white"} p-2 cursor-pointer rounded text-center fw-bold`}>Média</div>
                     {showInfoTask.includes(1) && media.map((task)=>{
                         return (
-                            <Task  key={task.id + task.title} done={done} onClick={showDetails} className={`${themeCtx.theme == 'dark' ? 'dark':'bg-light'} `} detailsShow={detailsShow}  task={task} />
+                            <Task onDragOver={(e)=> onDragOver(e)} onDropTask={(e) => onDropTask(e, task.id)} onDrag={(e)=> onDrag(e, task.id)} key={task.id + task.title} done={done} onClick={showDetails} className={`${themeCtx.theme == 'dark' ? 'dark':'bg-light'} `} detailsShow={detailsShow}  task={task} />
                         )
                     })}
                 </div>
                 }
                 {showOnly.includes(2) &&
-                <div className="col-lg-3 overflow-x-auto p-4 col-sm-6 bg-red-600">
+                <div onDrop={(e)=> onDrop(e, 2)} onDragOver={(e) => onDragOver(e) } className="col-lg-3 overflow-x-auto p-4 col-sm-6 bg-red-600">
                     <div onClick={()=> openInformation(2)} className={`${themeCtx.theme == 'dark' ? "bg-red-600":"bg-red-600 text-white"} p-2 cursor-pointer rounded text-center fw-bold`}>Alta</div>
                     {showInfoTask.includes(2) && alta.map((task)=>{
                         return (
-                            <Task key={task.id + task.title} done={done} onClick={showDetails} className={`${themeCtx.theme == 'dark' ? 'dark':'bg-light'} `} detailsShow={detailsShow}  task={task} />
+                            <Task onDragOver={(e)=> onDragOver(e)} onDropTask={(e) => onDropTask(e, task.id)} onDrag={(e)=> onDrag(e, task.id)} key={task.id + task.title} done={done} onClick={showDetails} className={`${themeCtx.theme == 'dark' ? 'dark':'bg-light'} `} detailsShow={detailsShow}  task={task} />
                         )
                     })}
                 </div>
